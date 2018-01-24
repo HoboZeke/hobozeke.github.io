cd2a645ab8ddd980232cfd43a4b79c19ae1413f2
HoboZeke <35333739+HoboZeke@users.noreply.github.com>
2018-01-24 11:00:24 +0000

    Update main.js

--- incremental/main.js
+++ incremental/main.js
@@ -1,7 +1,20 @@
 var seeds = 0;
+var seedHarvesters = 0;
 
 function seedClick (number)
 {
     seeds = seeds+number;
     document.getElementById("seeds").innerHTML = seeds;
 }
+
+function buySeedHarvester(){
+    var harvesterCost = Math.floor(10 * Math.pow(1.1,seedHarvesters));         //works out the cost of this cursor
+    if(seeds >= harvesterCost){                                                 //checks that the player can afford the cursor
+        seedHarvesters = seedHarvesters + 1;                                   //increases number of cursors
+    	seeds = seeds - harvesterCost;                                           //removes the cookies spent
+        document.getElementById('seedHarvesters').innerHTML = seedHarvesters;  //updates the number of cursors for the user
+        document.getElementById('seeds').innerHTML = seed;                       //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));                       //works out the cost of the next cursor
+    document.getElementById('harvesterCost').innerHTML = nextCost;               //updates the cursor cost for the user
+};
