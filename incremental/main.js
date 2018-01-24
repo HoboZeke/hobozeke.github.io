5858eb7b1b775a9c1c31ef96ef62e08bbbfbbade
HoboZeke <35333739+HoboZeke@users.noreply.github.com>
2018-01-24 12:21:06 +0000

    Update main.js

--- incremental/main.js
+++ incremental/main.js
@@ -16,14 +16,19 @@ function buySeedHarvester()
     	seeds = seeds - harvesterCost;                                           //removes the cookies spent
         document.getElementById('seedHarvesters').innerHTML = seedHarvesters;  //updates the number of cursors for the user
         document.getElementById('seeds').innerHTML = seeds;                       //updates the number of cookies for the user
-    };
+    }
     var nextCost = Math.floor(10 * Math.pow(1.1,seedHarvesters));                       //works out the cost of the next cursor
     document.getElementById('harvesterCost').innerHTML = nextCost;               //updates the cursor cost for the user
-};
+}
+
+function getRandomInt(max) 
+{
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function throwSeeds()
 {
-    var pigeonHunger = Math.floor(Math.random() * Math.floor(5)) + 1;
+    var pigeonHunger = getRandomInt(5) + 1;
     pigeons = seeds/pigeonHunger;
     seeds = 0;
     
