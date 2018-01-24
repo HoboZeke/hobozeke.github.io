b00c338d6c29a476613f2d2abcc0e3b8b98f0504
HoboZeke <35333739+HoboZeke@users.noreply.github.com>
2018-01-24 12:23:51 +0000

    Update main.js

--- incremental/main.js
+++ incremental/main.js
@@ -29,7 +29,7 @@ function getRandomInt(max)
 function throwSeeds()
 {
     var pigeonHunger = getRandomInt(5) + 1;
-    pigeons = seeds/pigeonHunger;
+    pigeons = seeds/5;
     seeds = 0;
     
     document.getElementById("pigeons").innerHTML = pigeons;
