8acfaa2f2dedcade629f01f9b580b4da6dba43ba
HoboZeke <35333739+HoboZeke@users.noreply.github.com>
2018-01-30 10:04:20 +0000

    Update main.js

--- incremental/main.js
+++ incremental/main.js
@@ -16,7 +16,6 @@ var breadRevealed = 0;
 
 function setup()
 {
-    Load();
     Hide("seedsButton");
     Hide("breadButton");
     document.getElementById("story").innerHTML = "You have no home, no money, no shoes. </br> You sit on a park bench looking at your one possession, a bag of seeds.";
