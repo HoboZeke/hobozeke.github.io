e7861294c95af25d005e744ba0f5e25cea1a52d3
HoboZeke <35333739+HoboZeke@users.noreply.github.com>
2018-02-10 11:20:47 +0000

    Update main.js

--- incremental/main.js
+++ incremental/main.js
@@ -13,34 +13,34 @@ var pigeonThreshold = 100;
 function setup()
 {
 	Load();
-    
-    	UpdateLabels("seeds");
-    	UpdateLabels("flour");
-    	UpdateLabels("days"); 
+
+	UpdateLabels("seeds");
+	UpdateLabels("flour");
+	UpdateLabels("days"); 
 	UpdateLabels("money");
-    
-    	if (loadedGameState == 0)
-    	{
+
+	if (loadedGameState == 0)
+	{
 		seeds = 25;
 		pigeonThreshold = 100;
 		Hide("seedSection");
-        	Hide("flourButton");
-        	Hide("breadButton");
-        	document.getElementById("story").innerHTML = "You have no home, no money, no shoes. </br> You sit on a park bench looking at your one possession, a bag of seeds.";
-    	}
-    	else if (loadedGameState == 1)
-    	{
-        	UpdateLabels("pigeons");
-        	if(bread>=1){breadRevealed = 1; UpdateLabels("bread");}
-        	if(fatPigeons>=1){fatPigeonsRevealed = 1; UpdateLabels("pigeons");}
-        	if(oldFatPigeons>=1){oldFatPigeonsRevealed = 1; UpdateLabels("pigeons");}
-        	var nextGCost = Math.floor(10 * Math.pow(1.1,seedGrinders));                       
-        	document.getElementById('grinderCost').innerHTML = nextGCost; 
-        	var nextHCost = Math.floor(2 * Math.pow(1.1,seedHarvesters));                       
-        	document.getElementById("harvesterCost").innerHTML = nextHCost;
-        	document.getElementById("story").innerHTML = "You return to your park bench.";
-    	}
-	
+		Hide("flourButton");
+		Hide("breadButton");
+		document.getElementById("story").innerHTML = "You have no home, no money, no shoes. </br> You sit on a park bench looking at your one possession, a bag of seeds.";
+	}
+	else if (loadedGameState == 1)
+	{
+		UpdateLabels("pigeons");
+		if(bread>=1){breadRevealed = 1; UpdateLabels("bread");}
+		if(fatPigeons>=1){fatPigeonsRevealed = 1; UpdateLabels("pigeons");}
+		if(oldFatPigeons>=1){oldFatPigeonsRevealed = 1; UpdateLabels("pigeons");}
+		var nextGCost = Math.floor(10 * Math.pow(1.1,seedGrinders));                       
+		document.getElementById("grinderCost").innerHTML = nextGCost; 
+		var nextHCost = Math.floor(2 * Math.pow(1.1,seedHarvesters));                       
+		document.getElementById("harvesterCost").innerHTML = nextHCost;
+		document.getElementById("story").innerHTML = "You return to your park bench.";
+	}
+
 	if (pigeonThreshold <= 99)
 	{
 		pigeonThreshold = 100;
@@ -49,13 +49,13 @@ function setup()
 
 function seedClick (number)
 {
-    	seeds = seeds+number;
-    	UpdateLabels("seeds");
+	seeds = seeds+number;
+	UpdateLabels("seeds");
 }
 function flourClick (number)
 {
-    	flour = flour+number;
-    	UpdateLabels("flour");
+	flour = flour+number;
+	UpdateLabels("flour");
 }
 function moneyClick (number)
 {
@@ -69,104 +69,104 @@ function moneyClick (number)
 
 function buySeedHarvester()
 {
-    	var harvesterCost = Math.floor(10 * Math.pow(1.1,seedHarvesters));         	//works out the cost of this cursor
-    	if(seeds >= harvesterCost){                                                	//checks that the player can afford the cursor
-        	seedHarvesters = seedHarvesters + 1;                                   	//increases number of cursors
-    		seeds = seeds - harvesterCost;                                          //removes the cookies spent
-        	UpdateLabels("seeds");                                                	//updates the number of cookies for the user
-    	}
-    	var nextCost = Math.floor(10 * Math.pow(1.1,seedHarvesters));              	//works out the cost of the next cursor
-    	document.getElementById('harvesterCost').innerHTML = nextCost;              	//updates the cursor cost for the user
+	var harvesterCost = Math.floor(10 * Math.pow(1.1,seedHarvesters));         	//works out the cost of this cursor
+	if(seeds >= harvesterCost){                                                	//checks that the player can afford the cursor
+		seedHarvesters = seedHarvesters + 1;                                   	//increases number of cursors
+		seeds = seeds - harvesterCost;                                          //removes the cookies spent
+		UpdateLabels("seeds");                                                	//updates the number of cookies for the user
+	}
+	var nextCost = Math.floor(10 * Math.pow(1.1,seedHarvesters));              	//works out the cost of the next cursor
+	document.getElementById("harvesterCost").innerHTML = nextCost;              	//updates the cursor cost for the user
 }
 
 function buySeedGrinders()
 {
-    	var grinderCost = Math.floor(10 * Math.pow(1.1,seedGrinders));         		//works out the cost of this cursor
-    	if(money >= grinderCost){                                                 	//checks that the player can afford the cursor
-        	seedGrinders = seedGrinders + 1;                                  	//increases number of cursors
-    		money = money - grinderCost;                                            //removes the cookies spent
-        	UpdateLabels("money");                                                	//updates the number of cookies for the user
-    	}
-    	var nextCost = Math.floor(10 * Math.pow(1.1,seedGrinders));                	//works out the cost of the next cursor
-    	document.getElementById('grinderCost').innerHTML = nextCost;               	//updates the cursor cost for the user
+	var grinderCost = Math.floor(10 * Math.pow(1.1,seedGrinders));         		//works out the cost of this cursor
+	if(money >= grinderCost){                                                 	//checks that the player can afford the cursor
+		seedGrinders = seedGrinders + 1;                                  	//increases number of cursors
+		money = money - grinderCost;                                            //removes the cookies spent
+		UpdateLabels("money");                                                	//updates the number of cookies for the user
+	}
+	var nextCost = Math.floor(10 * Math.pow(1.1,seedGrinders));                	//works out the cost of the next cursor
+	document.getElementById("grinderCost").innerHTML = nextCost;               	//updates the cursor cost for the user
 }
 
 function getRandomInt(max) 
 {
-  	return Math.floor(Math.random() * Math.floor(max));
+	return Math.floor(Math.random() * Math.floor(max));
 }
 
 function throwSeeds()
 {
 	Reveal("seedSection");
 	Hide("story");																	//Put this here to allow events later in list to create story items.
-    	var previousPigeons = pigeons;
-    
-    	var pigeonHunger = getRandomInt(5) + 1;
-    	pigeons = Math.floor(seeds/5);
-    	seeds = 0;
-    	if(interval == 1)																//Set the original pigeon count on day 1
-    	{
-        	persistantPigeons = pigeons;
-    	}
-    	pigeonDifference = pigeons - persistantPigeons;
-    	if (seeds > pigeonDifference)
-    	{
-        	persistantPigeons = pigeons;
-    	}
-    
-    	if (bread >= fatPigeons)
-    	{
-        	bread = bread - fatPigeons;
-    	}
-    	else
-    	{
-        	fatPigeons = bread;
-        	bread = 0;
-    	}
-    
-    	interval = interval + 1;
-    	if (interval == 8)
-    	{        
-        	oldFatPigeons = oldFatPigeons + fatPigeons;
-        	if (oldFatPigeons >= 1){oldFatPigeonsRevealed = 1;}
-        	fatPigeons = persistantPigeons;
-        	interval = 1;
-        	if (fatPigeons >= 1){fatPigeonsRevealed = 1;}
-    	}
-	
+	var previousPigeons = pigeons;
+
+	var pigeonHunger = getRandomInt(5) + 1;
+	pigeons = Math.floor(seeds/5);
+	seeds = 0;
+	if(interval == 1)																//Set the original pigeon count on day 1
+	{
+		persistantPigeons = pigeons;
+	}
+	pigeonDifference = pigeons - persistantPigeons;
+	if (seeds > pigeonDifference)
+	{
+		persistantPigeons = pigeons;
+	}
+
+	if (bread >= fatPigeons)
+	{
+		bread = bread - fatPigeons;
+	}
+	else
+	{
+		fatPigeons = bread;
+		bread = 0;
+	}
+
+	interval = interval + 1;
+	if (interval == 8)
+	{        
+		oldFatPigeons = oldFatPigeons + fatPigeons;
+		if (oldFatPigeons >= 1){oldFatPigeonsRevealed = 1;}
+		fatPigeons = persistantPigeons;
+		interval = 1;
+		if (fatPigeons >= 1){fatPigeonsRevealed = 1;}
+	}
+
 	if (pigeons >=  pigeonThreshold)
 	{
 		pigeonThreshold = pigeonThreshold*1.5;
 		StoryStage(1);
 	}
-    
-    	UpdateLabels("seeds");
-    	UpdateLabels("bread");
-    	UpdateLabels("days"); 
-    	UpdateLabels("pigeons");       
+
+	UpdateLabels("seeds");
+	UpdateLabels("bread");
+	UpdateLabels("days"); 
+	UpdateLabels("pigeons");       
 }
 
 function groundSeedsIntoFlour ()
 {
-    	flour = flour + seeds/10;
-    	seeds = 0;
-    
-    	UpdateLabels("seeds");
-    	UpdateLabels("flour");
+	flour = flour + seeds/10;
+	seeds = 0;
+
+	UpdateLabels("seeds");
+	UpdateLabels("flour");
 }
 
 function bakeBread ()
 {
-    	if (flour >= 100)
-    	{
-        	bread = bread + 1;
-        	flour = flour - 100;
-        	if (bread >= 1){breadRevealed = 1;}
-        
-        	UpdateLabels("flour");
-        	UpdateLabels("bread");
-    	}
+	if (flour >= 100)
+	{
+		bread = bread + 1;
+		flour = flour - 100;
+		if (bread >= 1){breadRevealed = 1;}
+
+		UpdateLabels("flour");
+		UpdateLabels("bread");
+	}
 }
 
 function StoryStage(stage)
@@ -191,107 +191,107 @@ function buyUpgrade(upgrade)
 
 function Reveal(name) 
 {
-    	var x = document.getElementById(name);
-    	if (x.style.display === "none") {
-        	x.style.display = "block";
-    	}
+	var x = document.getElementById(name);
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	}
 }
 function Hide(name) 
 {
-    	var x = document.getElementById(name);
-    	x.style.display = "none";
+	var x = document.getElementById(name);
+	x.style.display = "none";
 }
 
 function UpdateLabels (type)
 {
-    	if (type == "days"){
-        	document.getElementById("interval").innerHTML = interval;}
-	
+	if (type == "days"){
+		document.getElementById("interval").innerHTML = interval;}
+
 	if (type == "money"){
-        	document.getElementById("money").innerHTML = money.toFixed(2);}
-    
-    	if (type == "seeds"){
-        	document.getElementById("seeds").innerHTML = seeds;
-        	document.getElementById('seedHarvesters').innerHTML = seedHarvesters;}
-    
-    	if (type == "flour"){
-        	flour = Math.round(flour*10)/10; //Round the flour to 1 dp.
-        	document.getElementById("flour").innerHTML = flour.toFixed(2);
-        	document.getElementById('seedGrinders').innerHTML = seedGrinders;}
-    
-    	if (type == "bread"){
-        	if (breadRevealed == 1){
-            		document.getElementById("bread").innerHTML = bread + " loaves";}
-    	}
-    
-    	if (type == "pigeons"){
-        	document.getElementById("pigeons").innerHTML = pigeons + " pigeons currently feeding";
-        	if (fatPigeonsRevealed == 1){
-            		document.getElementById("fatPigeons").innerHTML = fatPigeons + " fat pigeons looking for bread";}
-        	if (oldFatPigeonsRevealed == 1){
-            		document.getElementById("oldFatPigeons").innerHTML = oldFatPigeons + " old fat pigeons pilfering pennies";}
-    	}
+		document.getElementById("money").innerHTML = money.toFixed(2);}
+
+	if (type == "seeds"){
+		document.getElementById("seeds").innerHTML = seeds;
+		document.getElementById("seedHarvesters").innerHTML = seedHarvesters;}
+
+	if (type == "flour"){
+		flour = Math.round(flour*10)/10; //Round the flour to 1 dp.
+		document.getElementById("flour").innerHTML = flour.toFixed(2);
+		document.getElementById("seedGrinders").innerHTML = seedGrinders;}
+
+	if (type == "bread"){
+		if (breadRevealed == 1){
+			document.getElementById("bread").innerHTML = bread + " loaves";}
+	}
+
+	if (type == "pigeons"){
+		document.getElementById("pigeons").innerHTML = pigeons + " pigeons currently feeding";
+		if (fatPigeonsRevealed == 1){
+			document.getElementById("fatPigeons").innerHTML = fatPigeons + " fat pigeons looking for bread";}
+		if (oldFatPigeonsRevealed == 1){
+			document.getElementById("oldFatPigeons").innerHTML = oldFatPigeons + " old fat pigeons pilfering pennies";}
+	}
 }
 
 window.setInterval(function()
-{
-    	seedClick(seedHarvesters);
-    	flourClick(seedGrinders);
-	
-	money += oldFatPigeons/100;
-	UpdateLabels("money");
-    
-    	if (flour >= 100)
-    	{
-        	Reveal("breadButton");
-    	}
-    
-    	if (seeds >= 50)
-    	{
-        	Reveal("flourButton");
-    	}
-    
-   	Save();
-    
-}, 1000);
+	{
+		seedClick(seedHarvesters);
+		flourClick(seedGrinders);
+
+		money += oldFatPigeons/100;
+		UpdateLabels("money");
+
+		if (flour >= 100)
+		{
+			Reveal("breadButton");
+		}
+
+		if (seeds >= 50)
+		{
+			Reveal("flourButton");
+		}
+
+		Save();
+
+	}, 1000);
 
 function Save ()
 {
-	localStorage.setItem('seeds',seeds);
-	localStorage.setItem('seedHarvesters',seedHarvesters);
-	localStorage.setItem('flour',flour);
-	localStorage.setItem('seedGrinders',seedGrinders);
-	localStorage.setItem('interval',interval);
-	localStorage.setItem('bread',bread);
-	localStorage.setItem('pigeons',pigeons);
-	localStorage.setItem('fatPigeons',fatPigeons);
-	localStorage.setItem('oldFatPigeons',oldFatPigeons);
-	localStorage.setItem('money',money);
-	localStorage.setItem('persistantPigeons',persistantPigeons);
-	localStorage.setItem('pigeonThreshold',pigeonThreshold);
+	localStorage.setItem("seeds",seeds);
+	localStorage.setItem("seedHarvesters",seedHarvesters);
+	localStorage.setItem("flour",flour);
+	localStorage.setItem("seedGrinders",seedGrinders);
+	localStorage.setItem("interval",interval);
+	localStorage.setItem("bread",bread);
+	localStorage.setItem("pigeons",pigeons);
+	localStorage.setItem("fatPigeons",fatPigeons);
+	localStorage.setItem("oldFatPigeons",oldFatPigeons);
+	localStorage.setItem("money",money);
+	localStorage.setItem("persistantPigeons",persistantPigeons);
+	localStorage.setItem("pigeonThreshold",pigeonThreshold);
 }
 
 function Load ()
 {
-    
-   	seeds = Number(localStorage.getItem("seeds"));
-    	seedHarvesters = Number(localStorage.getItem("seedHarvesters"));
-    	flour = Number(localStorage.getItem("flour"));
-    	seedGrinders = Number(localStorage.getItem("seedGrinders"));
-    	interval = 1; 						//Number(localStorage.getItem("interval")); Uncomment when day tracking is fixed for fatPigeons.
-    	bread = Number(localStorage.getItem("bread"));
-    	pigeons = Number(localStorage.getItem("pigeons"));
-    	fatPigeons = Number(localStorage.getItem("fatPigeons"));
-    	oldFatPigeons = Number(localStorage.getItem("oldFatPigeons"));
+
+	seeds = Number(localStorage.getItem("seeds"));
+	seedHarvesters = Number(localStorage.getItem("seedHarvesters"));
+	flour = Number(localStorage.getItem("flour"));
+	seedGrinders = Number(localStorage.getItem("seedGrinders"));
+	interval = 1; 						//Number(localStorage.getItem("interval")); Uncomment when day tracking is fixed for fatPigeons.
+	bread = Number(localStorage.getItem("bread"));
+	pigeons = Number(localStorage.getItem("pigeons"));
+	fatPigeons = Number(localStorage.getItem("fatPigeons"));
+	oldFatPigeons = Number(localStorage.getItem("oldFatPigeons"));
 	money = Number(localStorage.getItem("money"));
 	peristantPigeons = Number(localStorage.getItem("persistantPigeons"));
 	pigeonThreshold = Number(localStorage.getItem("pigeonThreshold"));
-    	if (pigeons >= 1){loadedGameState = 1}
+	if (pigeons >= 1){loadedGameState = 1}
 }
 
 function DeleteSave()
 {
-    	localStorage.removeItem("seeds");
+	localStorage.removeItem("seeds");
 	localStorage.removeItem("seedHarvesters");
 	localStorage.removeItem("flour");
 	localStorage.removeItem("seedGrinders");
@@ -303,5 +303,5 @@ function DeleteSave()
 	localStorage.removeItem("money");
 	localStorage.removeItem("persistantPigeons");
 	localStorage.removeItem("pigeonThreshold");
-    	setup();
+	setup();
 }
